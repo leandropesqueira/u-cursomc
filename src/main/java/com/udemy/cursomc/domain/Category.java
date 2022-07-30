6e9a262aada9cd06aeb7f6d8c7c5bc89d4234fb5
Leandro Pesqueira <leandropesqueira7@gmail.com>
2022-07-30 09:06:44 -0400

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/udemy/cursomc/domain/Category.java
+++ src/main/java/com/udemy/cursomc/domain/Category.java
@@ -2,17 +2,25 @@ package com.udemy.cursomc.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Category implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Integer id;
     private String name;
